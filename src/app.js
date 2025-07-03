e592d0baf8562c2899cfb5a87c4eacd3f548267f
NatiSen <ola.eufaloportugues@gmail.com>
2025-07-03 23:34:41 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'lost', 'destroyed', 'brokes'];
+  let what = ['my homework', 'my phone', 'the car', 'million dollars'];
+  let when = [
+    'before the event',
+    'when I was sleeping',
+    'yesterday',
+    'while I was taking a shower'
+  ];
+
+  const getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  const excuse =
+    getRandom(who) +
+    ' ' +
+    getRandom(action) +
+    ' ' +
+    getRandom(what) +
+    ' ' +
+    getRandom(when) +
+    '.';
+
+  document.getElementById('excuse').innerText = excuse;
 };
+
+  
+
